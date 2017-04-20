25c9a3a766f7431888f6767ebe5015475370930a
Amine Benseddik <amine.benseddik@gmail.com>
2017-04-20 11:46:02 +0200

    adding StackImpact profiling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/amine7536/quasar/cmd"
+	stackimpact "github.com/stackimpact/stackimpact-go"
 )
 
 const (
@@ -14,6 +15,12 @@ const (
 )
 
 func main() {
+
+	agent := stackimpact.NewAgent()
+	agent.Start(stackimpact.Options{
+		AgentKey: "66cf1969d58a40cdac6da1455a6dbffe8611efbb",
+		AppName:  ProgramName,
+	})
 
 	if err := cmd.NewRootCmd(Version, ProgramName).Execute(); err != nil {
 		log.Fatal(err)
