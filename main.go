c3f2c4c0948d586982b71fd68cce8630da653ea7
Amine Benseddik <amine.benseddik@gmail.com>
2017-04-21 10:34:48 +0200

    adding logstash output

--- main.go
+++ main.go
@@ -4,23 +4,16 @@ import (
 	"log"
 
 	"github.com/amine7536/quasar/cmd"
-	stackimpact "github.com/stackimpact/stackimpact-go"
 )
 
 const (
 	// Version : app version
-	Version = "0.1.0"
+	Version = "0.3.0"
 	// ProgramName : app name
 	ProgramName = "Quasar"
 )
 
 func main() {
-
-	agent := stackimpact.NewAgent()
-	agent.Start(stackimpact.Options{
-		AgentKey: "66cf1969d58a40cdac6da1455a6dbffe8611efbb",
-		AppName:  ProgramName,
-	})
 
 	if err := cmd.NewRootCmd(Version, ProgramName).Execute(); err != nil {
 		log.Fatal(err)
